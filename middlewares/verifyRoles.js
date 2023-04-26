7c4a507efd3635a8b66a943150ad7a6def63fb00
Chí Hiển <89452066+ALX-Hien2k2@users.noreply.github.com>
2023-04-26 16:31:32 +0700

    update

--- middlewares/verifyRoles.js
+++ middlewares/verifyRoles.js
@@ -1,9 +1,7 @@
 const verifyRoles = (...allowedRoles) => {
   return (req, res, next) => {
-    console.log("allowedRoles", allowedRoles)
     if (!req?.user.role) return res.sendStatus(401);
     const rolesArray = [...allowedRoles];
-    console.log("rolesArray", rolesArray)
     if (!rolesArray.includes(req.user.role)) return res.sendStatus(401);
     next();
   };
